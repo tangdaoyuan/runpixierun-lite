544f466be4afc180be556e04ff6d00c7b465c116
唐道源 <tangdaoyuan@megvii.com>
2022-06-23 21:07:33 +0800

    chore: half upgrade pixijs v6

---
+++ src/js/game/Steve.js
@@ -0,0 +1,247 @@
+import * as PIXI from '../pixi'
+import { gsap } from "gsap"
+
+const GAME = {};
+
+export {
+  GAME
+}
+
+GAME.Steve = function() {
+    this.position = new PIXI.Point();
+
+    this.runningFrames = [
+        PIXI.Texture.from("characterRUNscaled_01.png"),
+        PIXI.Texture.from("characterRUNscaled_02.png"),
+        PIXI.Texture.from("characterRUNscaled_03.png"),
+        PIXI.Texture.from("characterRUNscaled_04.png"),
+        PIXI.Texture.from("characterRUNscaled_05.png"),
+        PIXI.Texture.from("characterRUNscaled_06.png"),
+        PIXI.Texture.from("characterRUNscaled_07.png"),
+        PIXI.Texture.from("characterRUNscaled_08.png"),
+        PIXI.Texture.from("characterRUNscaled_09.png")
+    ];
+
+    this.flyingFrames = [
+        PIXI.Texture.from("characterFLATflying_01.png"),
+        PIXI.Texture.from("characterFLATflying_02.png"),
+        PIXI.Texture.from("characterFLATflying_03.png")
+    ];
+
+    this.crashFrames = [
+        PIXI.Texture.from("characterFALLscaled3.png"),
+        PIXI.Texture.from("characterFALLscaled1.png"),
+        PIXI.Texture.from("characterFALLscaled3.png")
+    ];
+
+    this.view = new PIXI.MovieClip(this.flyingFrames);
+    this.view.animationSpeed = 0.23;
+
+    this.view.anchor.x = 0.5;
+    this.view.anchor.y = 0.5;
+
+    this.position.y = 477;
+    this.ground = 477;
+    this.gravity = 0.3;
+
+    this.baseSpeed = 8;
+    this.speed = new PIXI.Point(this.baseSpeed, 0);
+
+    this.activeCount = 0;
+    this.isFlying = false;
+    this.accel = 0;
+
+    this.width = 26
+    this.height = 37;
+
+    this.onGround = false;
+    this.rotationSpeed = 0;
+    this.joyRiding = false;
+    this.level = 1;
+    this.realAnimationSpeed = 0.23;
+
+    this.volume = 0.3;
+}
+
+GAME.Steve.constructor = GAME.Steve;
+
+GAME.Steve.prototype.update = function() {
+    if (this.isDead) {
+        this.updateDieing();
+    } else {
+        this.updateRunning();
+    }
+}
+
+GAME.Steve.prototype.joyrideMode = function() {
+    this.joyRiding = true;
+    FidoAudio.setVolume('runRegular', 0);
+    FidoAudio.play('hyperMode');
+    gsap.to(this.speed, 0.3, {
+        x: 20,
+        ease: Cubic.easeIn
+    });
+    this.realAnimationSpeed = 0.23 * 4
+}
+
+GAME.Steve.prototype.normalMode = function() {
+    this.joyRiding = false;
+    FidoAudio.setVolume('runFast', 0);
+    if (this.onGround === true) FidoAudio.setVolume('runRegular', this.volume);
+    gsap.to(this.speed, 0.6, {
+        x: this.baseSpeed,
+        ease: Cubic.easeOut
+    });
+    this.realAnimationSpeed = 0.23;
+}
+
+GAME.Steve.prototype.updateRunning = function() {
+    this.view.animationSpeed = this.realAnimationSpeed * GAME.time.DELTA_TIME * this.level;
+
+    if (this.isActive) {
+        this.isFlying = true;
+    }
+
+    var oldSpeed = this.speed.y;
+
+    if (this.isFlying) {
+        this.accel = 0.6;
+        this.speed.y -= this.accel * GAME.time.DELTA_TIME;
+        if (this.speed.y > 0) this.speed.y -= 0.3 * GAME.time.DELTA_TIME;
+    } else {
+        if (this.speed.y < 0) this.speed.y += 0.05 * GAME.time.DELTA_TIME;
+    }
+
+    this.speed.y += this.gravity * GAME.time.DELTA_TIME;
+
+    if (this.speed.y > 8) this.speed.y = 8;
+    if (this.speed.y < -9) this.speed.y = -9;
+
+    var accel = this.speed.y - oldSpeed;
+    this.position.x += this.speed.x * GAME.time.DELTA_TIME * this.level;
+    this.position.y += this.speed.y * GAME.time.DELTA_TIME;
+
+    if (this.onGround !== this.onGroundCache) {
+        this.onGroundCache = this.onGround;
+
+        if (this.onGround) {
+            this.view.textures = this.runningFrames;
+            if (this.joyRiding === true) {
+                FidoAudio.setVolume('runFast', this.volume);
+                FidoAudio.setVolume('runRegular', 0);
+            } else {
+                FidoAudio.setVolume('runRegular', this.volume);
+                FidoAudio.setVolume('runFast', 0);
+            }
+        } else {
+            FidoAudio.setVolume('runFast', 0);
+            FidoAudio.setVolume('runRegular', 0);
+            this.view.textures = this.flyingFrames;
+        }
+    }
+
+    GAME.camera.x = this.position.x - 100;
+
+    this.view.position.x = this.position.x - GAME.camera.x;
+    this.view.position.y = this.position.y - GAME.camera.y;
+
+    this.view.rotation += (this.speed.y * 0.05 - this.view.rotation) * 0.1;
+}
+
+GAME.Steve.prototype.updateDieing = function() {
+    this.speed.x *= 0.999;
+
+    if (this.onGround) this.speed.y *= 0.99;
+
+    this.speed.y += 0.1;
+    this.accel += (0 - this.accel) * 0.1 * GAME.time.DELTA_TIME;
+
+    this.speed.y += this.gravity * GAME.time.DELTA_TIME;;
+
+    this.position.x += this.speed.x * GAME.time.DELTA_TIME;;
+    this.position.y += this.speed.y * GAME.time.DELTA_TIME;;
+
+    GAME.camera.x = this.position.x - 100;
+
+    this.view.position.x = this.position.x - GAME.camera.x;
+    this.view.position.y = this.position.y - GAME.camera.y;
+
+    if (this.speed.x < 5) {
+        this.view.rotation += this.rotationSpeed * (this.speed.x / 5) * GAME.time.DELTA_TIME;
+    } else {
+        this.view.rotation += this.rotationSpeed * GAME.time.DELTA_TIME;
+    }
+}
+
+GAME.Steve.prototype.jump = function() {
+    if (this.isDead) {
+        if (this.speed.x < 5) {
+            this.isDead = false
+            this.speed.x = 10;
+        }
+    }
+
+    if (this.position.y !== this.ground) {
+        this.isFlying = true;
+    } else {
+        this.isActive = true;
+        this.activeCount = 0;
+    }
+}
+
+GAME.Steve.prototype.die = function() {
+    if (this.isDead) return;
+
+    FidoAudio.setVolume('runFast', 0);
+    FidoAudio.setVolume('runRegular', 0);
+    FidoAudio.fadeOut('gameMusic');
+
+    gsap.to(GAME.time, 0.5, {
+        speed: 0.1,
+        ease: Cubic.easeOut,
+        onComplete: function() {
+            FidoAudio.play('deathJingle');
+            gsap.to(GAME.time, 2, {
+                speed: 1,
+                delay: 1
+            });
+        }
+    });
+
+    this.isDead = true;
+    this.bounce = 0;
+    this.speed.x = 15;
+    this.speed.y = -15;
+    this.rotationSpeed = 0.3;
+    this.view.stop();
+}
+
+
+GAME.Steve.prototype.boil = function() {
+    if (this.isDead) return;
+
+    FidoAudio.setVolume('runFast', 0);
+    FidoAudio.setVolume('runRegular', 0);
+    FidoAudio.fadeOut('gameMusic');
+    FidoAudio.play('lavaSplosh');
+    FidoAudio.play('deathJingle');
+
+    this.isDead = true;
+}
+
+GAME.Steve.prototype.fall = function() {
+    this.isActive = false;
+    this.isFlying = false;
+}
+
+GAME.Steve.prototype.isAirbourne = function() {}
+
+GAME.Steve.prototype.stop = function() {
+    this.view.stop();
+    FidoAudio.setVolume('runRegular', 0);
+}
+
+GAME.Steve.prototype.resume = function() {
+    this.view.play();
+    if (this.onGround) FidoAudio.setVolume('runRegular', this.volume);
+}
---
+++ src/js/game/StressTest.js
@@ -0,0 +1,163 @@
+// update
+import * as PIXI from '../pixi'
+
+
+export const Stress = {};
+
+Stress.StressTest = function(callback) {
+    this.Device = new Fido.Device();
+
+    this.width = window.innerWidth || document.body.clientWidth;
+    this.height = window.innerHeight || document.body.clientHeight;
+
+    this.frameCount = 0;
+    this.tick = 0;
+    this.loadingFrames = [
+        "/img/loading_01.png",
+        "/img/loading_02.png",
+        "/img/loading_03.png",
+        "/img/loading_04.png"
+    ];
+    this.currentLoadSprite = false;
+
+    var assetLoader = new PIXI.Loader()
+    assetLoader.add(this.loadingFrames);
+    assetLoader.load();
+
+    this.callback = callback;
+    this.renderer = new PIXI.CanvasRenderer({
+        width: this.width,
+        height: this.height,
+    });
+    this.stage = new PIXI.Stage(0x25284A);
+
+    this.graphics = new PIXI.Graphics();
+    this.graphics.beginFill(0x25284A);
+    this.graphics.drawRect(0, 0, this.width, this.height);
+    this.stage.addChild(this.graphics);
+
+    document.body.appendChild(this.renderer.view);
+
+    this.stage.touchstart = this.stage.mousedown = function(event) {
+        event.originalEvent.preventDefault();
+    }
+
+
+    this.duration = 3;
+
+    var scope = this;
+    PIXI.Texture.fromURL("/img/testImage.png").then(res => {
+      this.texture = res;
+      scope.begin();
+      this.frameRate = [];
+    });
+}
+
+// constructor
+Stress.StressTest.constructor = Stress.StressTest;
+
+Stress.StressTest.prototype.begin = async function() {
+    this.testSprites = [];
+    for (var i = 0; i < 300; i++) {
+        var bunny = new PIXI.Sprite(this.texture);
+
+        bunny.anchor.x = 0.5;
+        bunny.anchor.y = 0.5;
+        bunny.position.x = 50 + Math.random() * 400;
+        bunny.position.y = 10;
+
+        this.stage.addChild(bunny);
+        this.testSprites.push(bunny);
+    };
+
+    this.graphics2 = new PIXI.Graphics();
+    this.graphics2.beginFill(0x25284A);
+    this.graphics2.drawRect(0, 0, this.width, this.height);
+    this.stage.addChild(this.graphics2);
+
+    const r = await PIXI.Texture.fromURL('/img/goodboy_logo.png')
+    var logo = new PIXI.Sprite(r);
+    logo.anchor.x = 0.5;
+    logo.anchor.y = 0.5;
+    logo.position.x = this.width * 0.5;
+    logo.position.y = this.height * 0.48;
+
+
+    logo.scale.set(1);
+
+    this.stage.addChild(logo);
+
+    this.renderer.render(this.stage);
+
+    this.startTime = Date.now();
+    this.lastTime = Date.now();
+
+    var scope = this;
+    requestAnimFrame(function() {
+        scope.update();
+    });
+}
+
+Stress.StressTest.prototype.resize = function(w, h) {
+    this.width = w;
+    this.height = h;
+}
+
+Stress.StressTest.prototype.update = function() {
+    this.frameCount++;
+
+    if (this.frameCount % 12 === 1) {
+        if (this.tick === this.loadingFrames.length) {
+            this.tick = 0;
+        }
+
+        var sprite = SpritePool.getInstance().get(this.loadingFrames[this.tick])
+        sprite.anchor.x = 0.5;
+        sprite.anchor.y = 0.5;
+        sprite.position.x = this.width * 0.5;
+        sprite.position.y = this.height * 0.5 + 40;
+
+        this.stage.addChild(sprite);
+        if (this.currentLoadSprite !== false) this.stage.removeChild(this.currentLoadSprite);
+
+        this.currentLoadSprite = sprite;
+
+        this.tick++;
+    }
+
+    var currentTime = Date.now();
+
+    for (var i = 0; i < this.testSprites.length; i++) {
+        this.testSprites[i].rotation += 0.3;
+    }
+
+    this.renderer.render(this.stage);
+
+    var diff = currentTime - this.lastTime;
+    diff *= 0.06;
+
+    this.frameRate.push(diff);
+
+    this.lastTime = currentTime;
+
+    var elapsedTime = currentTime - this.startTime;
+
+    if (elapsedTime < this.duration * 1000) {
+        var scope = this;
+        requestAnimFrame(function() {
+            scope.update()
+        });
+    } else {
+        if (this.callback) this.callback();
+    }
+}
+
+Stress.StressTest.prototype.end = function() {
+    this.result = this.frameRate.length / this.duration;
+}
+
+Stress.StressTest.prototype.remove = function() {
+    document.body.removeChild(this.renderer.view);
+    this.cover = null;
+    this.renderer = null;
+}
