4fefd416919d51c9fbb6c348fed48f4a910cc940
唐道源 <tangdaoyuan@megvii.com>
2022-06-23 16:52:54 +0800

    chore: cleanup node modules

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
+import { gsap } from "gsap"
+import { ExpoScaleEase, RoughEase, SlowMo } from "gsap/EasePack";
 import './style/normalize.css'
 import './style/main.css'
+
+gsap.registerPlugin(ExpoScaleEase, RoughEase, SlowMo);
